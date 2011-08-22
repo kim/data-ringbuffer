c79df5f76a014b3316f4a177e6d6978b734dbfee
Kim Altintop <kim.altintop@gmail.com>
2011-08-22 23:16:19 +0200

    Use unsafeWrite on vector for slight performance improvement

--- src/Data/RingBuffer.hs
+++ src/Data/RingBuffer.hs
@@ -24,20 +24,16 @@ import qualified Data.Vector.Mutable as V
 -- import Debug.Trace
 
 
-data RingBuffer a = RingBuffer
-     {-# UNPACK #-} !(IORef Int64)   -- ^ cursor
-     {-# UNPACK #-} !(V.IOVector a)  -- ^ entries
-                    Int64            -- ^ size
-                    Int64            -- ^ ring mod mask (size - 1)
+data RingBuffer a = RingBuffer {-# UNPACK #-} !(IORef Int64)   -- ^ cursor
+                               {-# UNPACK #-} !(V.IOVector a)  -- ^ entries
+                                              Int64            -- ^ size
+                                              Int64            -- ^ ring mod mask (size - 1)
 
+data Consumer a = Consumer (a -> IO ())    -- ^ consuming function
+            {-# UNPACK #-} !(IORef Int64)  -- ^ consumer sequence
 
-data Consumer a = Consumer
-                  (a -> IO ())    -- ^ consuming function
-   {-# UNPACK #-} !(IORef Int64)  -- ^ consumer sequence
-
-data ProducerBarrier a = ProducerBarrier
-                         [Consumer a]    -- ^ consumers to track
-          {-# UNPACK #-} !(IORef Int64)  -- ^ producer sequence
+data ProducerBarrier a = ProducerBarrier [Consumer a]    -- ^ consumers to track
+                          {-# UNPACK #-} !(IORef Int64)  -- ^ producer sequence
 
 
 newRingBuffer :: Int -> a -> IO (RingBuffer a)
@@ -66,7 +62,7 @@ push (RingBuffer cursor entries l m)
      (ProducerBarrier consumers pseq) v = do
   i <- incrementAndGet pseq
   {-# SCC "inrange" #-} consumersInRange i
-  V.write entries (rmod i m) v
+  V.unsafeWrite entries (rmod i m) v
   writeIORef cursor i
   where
     consumersInRange i = minConsumerSeq >>= \mins ->
