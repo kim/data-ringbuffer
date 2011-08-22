aa255b2b6f01389219bfa41f688d147e91d9358f
Kim Altintop <kim.altintop@gmail.com>
2011-08-22 23:27:35 +0200

    Adjust some settings, prevent warnings

--- test/Perf.hs
+++ test/Perf.hs
@@ -9,20 +9,22 @@ import Data.RingBuffer
 import Data.Int
 import Data.IORef
 
-import Debug.Trace
+-- import Debug.Trace
 
 size :: Int
-size = 1024*32
+size = 1024*64
 
 iterations :: Int64
-iterations = 500000000
+-- iterations = 500000000
+-- iterations = 3000000
+iterations = 250000000
 
 main :: IO ()
 main = do
   consumer <- newConsumer (return . rnf)
   barrier  <- newProducerBarrier [consumer]
   buf      <- newRingBuffer size 0
-  done     <- newEmptyMVar
+  done     <- newEmptyMVar :: IO (MVar Int)
 
   forkIO $ produce buf barrier 0 >> print "producer done"
   forkIO $ consume' buf consumer `finally` putMVar done 1
